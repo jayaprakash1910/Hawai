960c6be59a1fb76de6db62594695dba873229019
Jayaprakash Gadela <jayaprakash_gadela@comcast.com>
2018-06-17 00:42:17 -0400

    updated logs

--- src/main/java/org/framework/utilities/DriverUtils.java
+++ src/main/java/org/framework/utilities/DriverUtils.java
@@ -9,22 +9,16 @@ public class DriverUtils extends Base {
 	
 	public static RemoteWebDriver getDriver(String driverType) throws Throwable {
 		if (driverType.equalsIgnoreCase("iOS") && EnvironmentUtils.isVirtualDevice() && EnvironmentUtils.isNative()) {
-			Log4j.info("Came in virtual ios 1" );
 			driver = AppiumiOSBase.createVirtualNativeAppiumDriver();
 		} else if (driverType.equalsIgnoreCase("iOS") && !EnvironmentUtils.isVirtualDevice() && EnvironmentUtils.isNative()) {
-			Log4j.info("Came in virtual ios 2");
 			driver = AppiumiOSBase.createRealSafariAppiumDriver();
 		} else if (driverType.equalsIgnoreCase("Android") && !EnvironmentUtils.isVirtualDevice() && !EnvironmentUtils.isNative()) {
-			Log4j.info("Came in virtual ios 3");
 			driver = AppiumAndroidBase.createRealChromeAppiumDriver();
 		} else if (driverType.equalsIgnoreCase("Desktop")) {
-			Log4j.info("Came in virtual ios 4");
 			driver = BrowserUtils.openBrowser("");
 		} else if (driverType.equalsIgnoreCase("Android") && EnvironmentUtils.isVirtualDevice() && EnvironmentUtils.isNative()) {
-			Log4j.info("Came in virtual ios 5");
 			driver = AppiumAndroidBase.createVirtualNativeAppiumDriver(deviceName);
 		} else if (driverType.equalsIgnoreCase("Android") && !EnvironmentUtils.isVirtualDevice() && EnvironmentUtils.isNative()) {
-			Log4j.info("Came in virtual ios 6");
 			driver = AppiumAndroidBase.createRealNativeAppiumDriver();
 		}
 		return driver;
