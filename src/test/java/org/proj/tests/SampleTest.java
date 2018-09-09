b92342dc9f00d32e921ddc3165dba0a1bf9ce205
Jayaprakash Gadela <jayaprakash_gadela@comcast.com>
2018-09-08 23:14:06 -0400

    updated sample test

--- src/test/java/org/proj/tests/SampleTest.java
+++ src/test/java/org/proj/tests/SampleTest.java
@@ -1,10 +1,33 @@
 package org.proj.tests;
 
-public class SampleTest {
+import static org.testng.Assert.assertTrue;
 
-	public static void main(String[] args) {
+import org.framework.Base;
+import org.framework.utilities.BrowserUtils;
+import org.testng.annotations.Test;
+
+public class SampleTest extends Base {
+
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	}*/
+	
+	@Test(groups = { "SampleTest" })
+    public void launchYahooMail() throws Throwable {
+		BrowserUtils.openBrowser("http://www.yahoo.com");
+		logHelper.info("Yahoo Home page is launched");
+		driver.findElementByCssSelector("#uh-signin").click();
+		Thread.sleep(3000);
+		logHelper.info("Yahoo email login page opened");
+		
+    }
+	
+	@Test(groups = { "SampleTest" })
+    public void verifyYahooLogo() throws Throwable {
+		BrowserUtils.openBrowser("http://www.yahoo.com");
+		assertTrue(driver.findElementByCssSelector("#uh-logo").isDisplayed());
+		logHelper.info("Yahoo Logo is visible");
+    }
 
 }
